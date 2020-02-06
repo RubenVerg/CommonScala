9b2441caf36d0ea064b51cc73274ca4464616e1d
Ruben Vergani <RubenVerg@outlook.com>
2020-02-06 15:00:41 +0100

    Update main.scala

--- main.scala
+++ main.scala
@@ -1,8 +1,23 @@
 package bz.x10.rubenverg.common
 
-object EasySearchXML {
+object ImplicitClasses {
   implicit class BetterString(val s: String) {
     def @(e: xml.Elem) {e \\ s}
     def <>(e: xml.Elem) {e \ s}
   }
+}
+
+object TestsClasses {
+  case class Person(val name = "", val surname = "", val age: Short = 0, val weight = 0.0)
+  val pizza = <pizza>
+    <meta type="crust">thin</meta>
+    <meta type="height" unit="cm">2</meta>
+    <content>
+      <meta type="pizzaType">Sausage</meta>
+      <item>Cheese</item>
+      <item>Tomato</item>
+      <item>Sausage</item>
+    </content>
+    <price value="7.50" unit="_EUR" />
+  </pizza>
 }
