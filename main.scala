3a61e04352e6a14b9b070c617b7e539a90662d25
Ruben Vergani <RubenVerg@outlook.com>
2020-02-13 17:12:32 +0100

    Update main.scala

--- main.scala
+++ main.scala
@@ -1,13 +1,19 @@
 package bz.x10.rubenverg.common
 
+//Uses ScalaXML
+//Uses Akka
+
 object ImplicitClasses {
   implicit class BetterString(val s: String) {
     def @(e: xml.Elem) {e \\ s}
     def <>(e: xml.Elem) {e \ s}
   }
+  implicit class BetterAny(val e: Any) {
+    def -->(r: akka.ActorRef) {r ! e}
+  }
 }
 
-object TestsClasses {
+object TestingClasses {
   case class Person(val name = "", val surname = "", val age: Short = 0, val weight = 0.0)
   val pizza = <pizza>
     <meta type="crust">thin</meta>
